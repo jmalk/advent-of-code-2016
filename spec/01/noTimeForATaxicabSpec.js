8a660dbe05b41855f5d270a749ff6217b5248bb5
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2016-12-15 15:21:02 +0000

    Naively pass first test

--- spec/01/noTimeForATaxicabSpec.js
+++ spec/01/noTimeForATaxicabSpec.js
@@ -1,4 +1,5 @@
 var rectilinearDistanceFromOrigin = require('../../src/01/rectilinearDistanceCalculator')
+var distanceToFirstCrossedPath = require('../../src/01/distanceToFirstCrossedPath')
 
 describe('Rectilinear distance calculator', function () {
   it('Takes a set of instructions and tells you how far they will take you from your location', function () {
@@ -13,5 +14,13 @@ describe('Rectilinear distance calculator', function () {
     instructions = 'L2'
     distance = rectilinearDistanceFromOrigin(instructions)
     expect(distance).toBe(2)
+  })
+})
+
+describe('Path cross detector', function () {
+  it('Finds the first point at which you cross your path', function () {
+    var instructions = 'R8, R4, R4, R8'
+    var distance = distanceToFirstCrossedPath(instructions)
+    expect(distance).toBe(4)
   })
 })
