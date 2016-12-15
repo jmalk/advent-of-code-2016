3dd2157caf853164674df7b5d8bbfa7946125066
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2016-12-15 15:23:35 +0000

    Use path.join

--- src/01/noTimeForATaxicab.js
+++ src/01/noTimeForATaxicab.js
@@ -1,6 +1,7 @@
 var fs = require('fs')
+var path = require('path')
 var rectilinearDistanceCalculator = require('./rectilinearDistanceCalculator')
 
-var answer = rectilinearDistanceCalculator(fs.readFileSync(__dirname + '/input.txt', 'utf-8'))
+var answer = rectilinearDistanceCalculator(fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf-8'))
 
 console.log('Day 1, part 1:', answer)
